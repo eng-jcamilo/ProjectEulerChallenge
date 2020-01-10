6a6dade19e06e2601b0bbf07a00c8aee85871873
Juan Camilo <eng.jcamilo@gmail.com>
2020-01-10 16:42:36 -0500

    Problem 2 short modification.

--- P2_evenfibonaccinumbers.py
+++ P2_evenfibonaccinumbers.py
@@ -1,14 +1,15 @@
 """ Project 2: Even Fibonacci numbers """
 
 def fiboEvenSum(n):
-    SEED_1 = 1
-    SEED_2 = 2
+    SEED_1, SEED_2 = 1, 2
     fibonacci = [SEED_1, SEED_2]
 
-    # First step: Generate Fibonacci list.
+    # First step: Generate Fibonacci list in a generic way.
+    # This version is not including four million value upper limit.
     for i in range(2, n):
         fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
 
+    print(fibonacci)
     # Second step: Obtain even numbers inside Fibonacci list.
     even = [val for val in fibonacci if val % 2 == 0]
 
